c0ee7cb40b63d7c2fb70d4ef67d8a23b048ce475
aminat <aminatalade33@gmail.com>
2026-06-29 09:52:48 -0700

    Complete JavaScript beginner assignment tasks

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Hello JavaScript!");
+console.log("My name is Aminat.");
+console.log("I am learning JavaScript.");
+const name = "Aminat";
+let age = 17;
+const favoriteFood = Noodles;
+const favoriteProgrammingLanguage = "JavaScript";
+const isStudent = true;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favorite Food:", favoriteFood);
+console.log("Favorite Programming Language:", favoriteProgrammingLanguage);
+console.log("Student:", isStudent);
