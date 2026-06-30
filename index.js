3a6bae3f9ad6e4ad54094219a40f4ac486b0f7dc
aminat <aminatalade33@gmail.com>
2026-06-29 22:22:40 -0700

    Complete JavaScript beginner assignment

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+// Print welcome messages to the console
 console.log("Hello JavaScript!");
 console.log("My name is Aminat.");
 console.log("I am learning JavaScript.");
+
+// Store personal information in variables
 const name = "Aminat";
 let age = 17;
-const favoriteFood = Noodles;
+const favoriteFood = "Noodles";
 const favoriteProgrammingLanguage = "JavaScript";
 const isStudent = true;
 
+// Display the values of the variables in the console
 console.log("Name:", name);
 console.log("Age:", age);
 console.log("Favorite Food:", favoriteFood);
