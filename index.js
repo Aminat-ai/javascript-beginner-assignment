a251c08dfb05e82f5a890d7d1a11593ebaba652c
aminat <aminatalade33@gmail.com>
2026-07-02 11:43:24 -0700

    Add Task 3 data types

--- index.js
+++ index.js
@@ -16,3 +16,26 @@ console.log("Age:", age);
 console.log("Favorite Food:", favoriteFood);
 console.log("Favorite Programming Language:", favoriteProgrammingLanguage);
 console.log("Student:", isStudent);
+
+// Task 3: Data Types
+
+const myName = "Aminat"; // String
+const myAge = 17; // Number
+const studentStatus = true; // Boolean
+let futureJob; // Undefined
+const emptyValue = null; // Null
+
+console.log(myName);
+console.log(typeof myName);
+
+console.log(myAge);
+console.log(typeof myAge);
+
+console.log(studentStatus);
+console.log(typeof studentStatus);
+
+console.log(futureJob);
+console.log(typeof futureJob);
+
+console.log(emptyValue);
+console.log(typeof emptyValue);
